b4f0255e4a0a0906be0114cf16c04ef5955536f6
Kim Ma <kimma1261@gmail.com>
2019-05-30 21:58:31 -0400

    Updates

---
+++ precache-manifest.9fa7085569a661810a91f37029a624ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdbf62828799679adc06",
+    "url": "/stackathon-spotify/static/js/runtime~main.00ccb8d8.js"
+  },
+  {
+    "revision": "06e462304b672af8eb5d",
+    "url": "/stackathon-spotify/static/js/main.5b39d615.chunk.js"
+  },
+  {
+    "revision": "63ecf72fcf2026257fc5",
+    "url": "/stackathon-spotify/static/js/2.4cc14643.chunk.js"
+  },
+  {
+    "revision": "06e462304b672af8eb5d",
+    "url": "/stackathon-spotify/static/css/main.f7aa0536.chunk.css"
+  },
+  {
+    "revision": "63ecf72fcf2026257fc5",
+    "url": "/stackathon-spotify/static/css/2.212efb81.chunk.css"
+  },
+  {
+    "revision": "f85f26acfa5fd97b32ac6721ae1d0c1f",
+    "url": "/stackathon-spotify/index.html"
+  }
+];
